ed8d70070054a1d7a6c8ac1c35e2a05f2f06098e
Deniz Lapsekili <71494496+3arii@users.noreply.github.com>
2023-12-19 05:33:28 -0800

    Update robotics.py

--- Robotics/robotics.py
+++ Robotics/robotics.py
@@ -9,13 +9,11 @@ env.launch(realtime=True)
 panda = rtb.models.Panda()
 panda.q = panda.qr
 
+env.add(panda)
 
 # Task 4: Get the Robot to move at a specific velocity vector
-def move_robot(dt, velocity_list):
+def move_robot(dt, velocity_list, arrived = False):
     Tep = panda.fkine(panda.q) * sm.SE3.Trans(velocity_list[0], velocity_list[1], velocity_list[2])
-
-    arrived = False
-    env.add(panda)
 
     while not arrived:
         v, arrived = rtb.p_servo(panda.fkine(panda.q), Tep, 1)
@@ -23,7 +21,28 @@ def move_robot(dt, velocity_list):
         env.step(dt)
 
 # Task 4 Example
-example_list = [0.2, 0.2, 0.45]
-move_robot(0.05, example_list)
+#example_list = [0.2, 0.2, 0]
+#move_robot(0.05, example_list)
+
+# ISSUES: The robot goes to the first specified velocity vector, and makes it way to the second one.
+# even after it reached the second one, it keeps on moving and does unpredictable behaviour. After some
+# debugging, I have determined that the issue is with the arrived varible as it doesn't change to true
+# after the first instance, but I have not been able to fix it.
+
+# Task 5: Get to robot to move to different velocity vectors
+def multiple_movement(dt, mulitple_velocity_list):
+    for velocity_vector in mulitple_velocity_list:
+        Tep = panda.fkine(panda.qr) * sm.SE3.Trans(velocity_vector[0], velocity_vector[1], velocity_vector[2])
+
+        arrived = False
+
+        while not arrived:
+            v, arrived = rtb.p_servo(panda.fkine(panda.qr), Tep, 1)
+            panda.qd = np.linalg.pinv(panda.jacobe(panda.qr)) @ v
+            env.step(dt)
+
+# Task 5 Example
+example_list_2 = [[0.2, 0.2, 0], [0.2, 0.2, 0.45]]
+multiple_movement(0.05, example_list_2)
 
 env.hold()
