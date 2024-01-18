f1c08892291e732c686832d1f016a9fdd1be0131
Deniz Lapsekili <71494496+3arii@users.noreply.github.com>
2024-01-18 14:26:32 -0800

    Fixed the issue of robot twitches

--- Robotics/robotics.py
+++ Robotics/robotics.py
@@ -2,6 +2,10 @@ import swift
 import roboticstoolbox as rtb
 import spatialmath as sm
 import numpy as np
+import csv
+
+velocity_vector_list = []
+filename = "data.csv"
 
 env = swift.Swift()
 env.launch(realtime=True)
@@ -21,28 +25,32 @@ def move_robot(dt, velocity_list, arrived = False):
         env.step(dt)
 
 # Task 4 Example
-#example_list = [0.2, 0.2, 0]
-#move_robot(0.05, example_list)
+example_list = [0.2, 0.2, 0]
 
-# ISSUES: The robot goes to the first specified velocity vector, and makes it way to the second one.
-# even after it reached the second one, it keeps on moving and does unpredictable behaviour. After some
-# debugging, I have determined that the issue is with the arrived varible as it doesn't change to true
-# after the first instance, but I have not been able to fix it.
 
-# Task 5: Get to robot to move to different velocity vectors
-def multiple_movement(dt, mulitple_velocity_list):
-    for velocity_vector in mulitple_velocity_list:
-        Tep = panda.fkine(panda.qr) * sm.SE3.Trans(velocity_vector[0], velocity_vector[1], velocity_vector[2])
-
+def multiple_movement(dt, multiple_velocity_list):
+    for velocity_vector in multiple_velocity_list:
+        Tep = panda.fkine(panda.q) * sm.SE3.Trans(velocity_vector[0], velocity_vector[1], velocity_vector[2])
         arrived = False
 
         while not arrived:
-            v, arrived = rtb.p_servo(panda.fkine(panda.qr), Tep, 1)
-            panda.qd = np.linalg.pinv(panda.jacobe(panda.qr)) @ v
+            v, arrived = rtb.p_servo(panda.fkine(panda.q), Tep, gain=1.0, threshold=0.2)  # Adjusted threshold
+            panda.qd = np.linalg.pinv(panda.jacobe(panda.q)) @ v
             env.step(dt)
 
+            velocity_vector_list.append(v.tolist())
+            with open(filename, 'w', newline='') as file:
+                writer = csv.writer(file)
+
+                # Write each row from the array
+                for row in velocity_vector_list:
+                    print(row)
+                    writer.writerow(row)  
+    print(f"Data has been written to {filename}")              
+
+
 # Task 5 Example
-example_list_2 = [[0.2, 0.2, 0], [0.2, 0.2, 0.45]]
+example_list_2 = [[0.2, 0.2, 0] , [0.1, 0.1, 0.45]]
 multiple_movement(0.05, example_list_2)
 
 env.hold()
